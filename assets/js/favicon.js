b98008283e1a1b705e4c24a57e09104c90d044fc
juanheuforico95 <juanheuforico@gmail.com>
2026-07-07 20:13:00 -0600

    add fav js module

---
+++ assets/js/favicon.js
@@ -0,0 +1,13 @@
+(function () {
+
+    const faviconHTML = `
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon2-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/icono/favicon-16x16.png">
+        <link rel="shortcut icon" href="assets/img/icono/favicon.ico">
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/img/icono/apple-touch-icon.png">
+        <link rel="manifest" href="assets/img/icono/site.webmanifest">
+    `;
+
+    document.head.insertAdjacentHTML("beforeend", faviconHTML);
+
+})();
